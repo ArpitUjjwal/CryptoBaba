f83b8ee51492832a20c1a271c66ccc7220923272
Arpit <arpitujjwal1@gmail.com>
2021-11-04 13:30:14 +0530

    init

---
+++ src/Components/Header.js
@@ -0,0 +1,52 @@
+import { AppBar, Container, createTheme, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import React from 'react'
+import { useHistory } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: "gold",
+        fontFamily: "Montserrat",
+        fontWeight: "bold", 
+        cursor: "pointer",
+    },
+}));
+
+const Header = () => {
+
+    const classes = useStyles()
+    const history = useHistory();
+
+    const{ currency, setCurrency, symbol } = CryptoState()
+    
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            type: "dark",
+        },
+    });
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <AppBar color='transparent' position='static'>
+                <Container>
+                    <Toolbar>
+                        <Typography onClick={() => history.push("/")} className={classes.title} variant='h6'>
+                            CryptoBaba
+                        </Typography>
+                        <Select variant="outlined" style={{width: 100, height:40, marginRight: 15}} value={currency} onChange={(e) => setCurrency(e.target.value)}>
+                            <MenuItem value={"USD"}>{symbol} USD</MenuItem>
+                            <MenuItem value={"INR"}>{symbol} INR</MenuItem>
+                        </Select>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
+    )
+}
+
+export default Header
